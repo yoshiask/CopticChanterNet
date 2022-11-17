9a34ecf63da8f12bb2eab210c0eef9c6a8e54a08
Yoshi Askharoun <jjask7@gmail.com>
2022-11-17 11:34:07 -0600

    Add project files.

---
+++ CopticChanterNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CopticChanterNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
