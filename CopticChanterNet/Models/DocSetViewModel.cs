0a8dc4b5bfe4611fb9b9c6079a3d75a3bccaf593
Yoshi Askharoun <jjask7@gmail.com>
2022-11-18 00:28:15 -0600

    Implemented crude horizontal document layout

--- CopticChanterNet/Models/DocSetViewModel.cs
+++ CopticChanterNet/Models/DocSetViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.DependencyInjection;
 using CommunityToolkit.Mvvm.Input;
 using CoptLib.IO;
 using CoptLib.Models;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CopticChanterNet.Models;
@@ -28,6 +31,9 @@ public partial class DocSetViewModel
     private ObservableCollection<Doc> docs;
 
     [ObservableProperty]
+    private List<List<ContentPart>> layout;
+
+    [ObservableProperty]
     private string title;
 
     private async Task LoadSetAync()
@@ -40,6 +46,7 @@ public partial class DocSetViewModel
             return;
 
         Docs = new();
+        Layout = new();
         foreach (var fileInfo in fileInfos)
         {
             using var file = fileInfo.CreateReadStream();
@@ -50,8 +57,43 @@ public partial class DocSetViewModel
 
             DocReader.ApplyDocTransforms(doc);
             docs.Add(doc);
+
+            Layout.AddRange(GenerateLayout(doc));
         }
 
         Title = "Midnight Praises";
+    }
+
+    private List<List<ContentPart>> GenerateLayout(Doc doc)
+    {
+        int translationCount = doc.Translations.Children.Count;
+
+        // Create rows for each stanza
+        int numRows = doc.Translations?.CountRows() ?? 0;
+        List<List<ContentPart>> layout = new(numRows);
+        for (int i = 0; i <= numRows; i++)
+            layout.Add(new(translationCount));
+
+        for (int t = 0; t < translationCount; t++)
+        {
+            GenerateLayoutForContentPart(doc.Translations![t], layout, t, 0);
+        }
+
+        return layout;
+    }
+
+    private void GenerateLayoutForContentPart(ContentPart part, List<List<ContentPart>> layout, int column, int row)
+    {
+        switch (part)
+        {
+            case IContent content:
+                layout[row].Add(part);
+                break;
+
+            case IContentCollectionContainer contentCollection:
+                foreach (var content in contentCollection.Children)
+                    GenerateLayoutForContentPart(content, layout, column, ++row);
+                break;
+        }
     }
 }
