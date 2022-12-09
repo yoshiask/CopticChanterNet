caca63fff9da472b6799a973f43451fbbc462777
Yoshi Askharoun <jjask7@gmail.com>
2022-12-08 23:00:44 -0600

    Added page for converting Tasbeha.org HTML to a Doc

--- CopticChanterNet/Controllers/HomeController.cs
+++ CopticChanterNet/Controllers/HomeController.cs
@@ -29,6 +29,11 @@ namespace CopticChanterNet.Controllers
             return View();
         }
 
+        public IActionResult TasbehaOrgGenerator()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ CopticChanterNet/Views/TasbehaOrg/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CopticChanterNet.Views.TasbehaOrg
+{
+    public partial class IndexModel : PageModel
+    {
+        public string? Html { get; set; }
+
+        public string? LyricId { get; set; }
+    }
+}
