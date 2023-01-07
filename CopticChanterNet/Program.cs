cc0ebc37fe797c85a4916a7336a52b6d30714d5d
Yoshi Askharoun <jjask7@gmail.com>
2023-01-06 18:25:26 -0600

    Disabled HTTPS redirection

--- CopticChanterNet/Program.cs
+++ CopticChanterNet/Program.cs
@@ -24,7 +24,7 @@ namespace CopticChanterNet
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
