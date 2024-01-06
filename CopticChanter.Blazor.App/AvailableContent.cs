34e0ae6cfa80c05008eb2051645c9358d7f847e8
Yoshi Askharoun <jjask7@gmail.com>
2024-01-05 19:46:15 -0600

    Port basic doc styles and components

---
+++ CopticChanter.Blazor.App/AvailableContent.cs
@@ -0,0 +1,8 @@
+﻿namespace CopticChanter.Blazor.App;
+
+public class AvailableContent
+{
+    public Dictionary<string, string> Docs { get; set; }
+    public Dictionary<string, string> Sequences { get; set; }
+    public Dictionary<string, string> Sets { get; set; }
+}
--- CopticChanter.Blazor.App/Program.cs
+++ CopticChanter.Blazor.App/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CopticChanter.Blazor.App;
+using System.Net.Http.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
