4ef725065e3d5a03cb179b0eeb8429041d551132
Yoshi Askharoun <jjask7@gmail.com>
2024-02-27 10:58:15 -0600

    Add new API project

---
+++ CopticChanter.WebApi/AvailableContent.cs
@@ -0,0 +1,41 @@
+﻿namespace CopticChanter.WebApi;
+
+public static class AvailableContent
+{
+    public static IReadOnlyDictionary<string, string> Docs = new Dictionary<string, string>
+    {
+        ["28c70071-ce5e-4add-aa5c-d093acfb2787"] = "The Morning Doxology.xml",
+        ["urn:tasbehaorg-cr:471"] = "The Thursday Theotokia (suscopt).xml",
+    };
+
+    public static IReadOnlyDictionary<string, string> Sequences = new Dictionary<string, string>
+    {
+        ["midnight-praises"] = "midnight-praises-sequence.xml",
+        ["nativity-paramoun"] = "nativity-paramoun-sequence.xml",
+    };
+
+    public static IReadOnlyDictionary<string, string> Sets = new Dictionary<string, string>
+    {
+        ["apostles-fast"] = "apostles-fast.zip",
+    };
+
+    public static string GetDocPath(string id) => Path.Combine("content/Docs", Docs[id]);
+    public static string GetSetPath(string id) => Path.Combine("content/Sets", Sets[id]);
+    public static string GetSequencePath(string id) => Path.Combine("content/Sequences", Sequences[id]);
+
+    public static string GetPath(string type, string id)
+    {
+        return type.ToUpperInvariant() switch
+        {
+            "DOC" => GetDocPath(id),
+            "SET" => GetSetPath(id),
+            "SEQ" => GetSequencePath(id),
+            _ => throw new ArgumentException(null, nameof(type))
+        };
+    }
+
+    public static Stream Open(string type, string id, IWebHostEnvironment env)
+    {
+        return env.WebRootFileProvider.GetFileInfo(GetPath(type, id)).CreateReadStream();
+    }
+}
---
+++ CopticChanter.WebApi/Controllers/LayoutController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using CoptLib.IO;
+using CoptLib.Models;
+using CoptLib.Writing;
+using Microsoft.AspNetCore.Mvc;
+using NodaTime;
+
+namespace CopticChanter.WebApi.Controllers;
+
+[Route("/layout")]
+public class LayoutController : Controller
+{
+    private static readonly ConcurrentDictionary<string, ILoadContext> _sessions = new();
+    private readonly IWebHostEnvironment _env;
+
+    public LayoutController(IWebHostEnvironment env)
+    {
+        _env = env;
+    }
+    
+    [Route("{type}/{id}")]
+    [HttpGet]
+    public async Task<IActionResult> GetLayout(string type, string id, [FromQuery] string? sessionKey,
+        [FromBody] Options? options)
+    {
+        sessionKey ??= Guid.NewGuid().ToString("N").ToUpper();
+        
+        var context = _sessions.GetOrAdd(sessionKey, _ => new LoadContext());
+        if (options?.Date is not null)
+            context.SetDate(LocalDateTime.FromDateTime(options.Date.Value));
+
+        type = type.ToUpperInvariant();
+        var stream = new Lazy<Stream>(() => AvailableContent.Open(type, id, _env));
+
+        List<List<IDefinition>> table = new();
+        if (type == "DOC")
+        {
+            var doc = context.LookupDefinition(id) as Doc
+                ?? context.LoadDoc(stream.Value);
+            
+            var docLayout = new DocLayout(doc, new(excludedLanguages: options?.ExcludedLanguages));
+            table = docLayout.CreateTable();
+        }
+
+        XElement xResponse = new("Layout");
+        xResponse.SetAttributeValue("Session", sessionKey);
+        foreach (var row in table)
+        {
+            XElement xRow = new("Row");
+            foreach (var def in row)
+            {
+                var cell = DocWriter.SerializeObject(def);
+                xRow.Add(cell);
+            }
+            xResponse.Add(xRow);
+        }
+        
+        XDocument xDoc = new();
+        xDoc.Add(xResponse);
+
+        StringBuilder sb = new();
+        await using var xmlWriter = XmlWriter.Create(sb, new XmlWriterSettings
+        {
+            Async = true,
+        });
+        await xDoc.WriteToAsync(xmlWriter, default);
+        await xmlWriter.FlushAsync();
+        
+        return Content(sb.ToString(), "application/xml");
+        //return Ok(new { Key = sessionKey, Table = table });
+    }
+
+    public record Options(DateTime? Date, List<string> ExcludedLanguageTags)
+    {
+        public IEnumerable<LanguageInfo> ExcludedLanguages = ExcludedLanguageTags.Select(LanguageInfo.Parse);
+    }
+}
