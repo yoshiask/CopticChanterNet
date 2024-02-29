ebef80ccdcc8eaed2e198ce5c231cfb117aef3c6
Yoshi Askharoun <jjask7@gmail.com>
2024-02-28 21:22:00 -0600

    Add Client library

---
+++ CopticChanter.WebApi.Client/CoptClient.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+using CopticChanter.WebApi.Core;
+using Flurl;
+using Flurl.Http;
+
+namespace CopticChanter.WebApi.Client;
+
+public class CoptClient(Url? baseUrl = null)
+{
+    public const string DefaultBaseUrl = "https://coptic.askharoun.com/api";
+
+    public Url BaseUrl { get; } = baseUrl ?? DefaultBaseUrl;
+
+    public IFlurlRequest GetBase() => new FlurlRequest(BaseUrl);
+
+    public async Task<AvailableContent> GetAvailableContentAsync()
+        => await GetBase().AppendPathSegment("content").GetJsonAsync<AvailableContent>();
+
+    public async Task<Layout> GetLayoutAsync(string type, string id, string? sessionKey)
+    {
+        var response = await GetBase()
+            .AppendPathSegments("layout", type, id)
+            .SendUrlEncodedAsync(HttpMethod.Get, null!)
+            .ReceiveStream();
+        
+        var xml = XDocument.Load(response);
+        return LayoutReaderWriter.FromXml(xml);
+    }
+}
