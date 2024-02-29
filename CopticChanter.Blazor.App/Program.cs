25b7e46deb2c092d1ae684ffe7ef30dc8d1d1897
Yoshi Askharoun <jjask7@gmail.com>
2024-02-28 21:31:07 -0600

    Use default API base

--- CopticChanter.Blazor.App/Program.cs
+++ CopticChanter.Blazor.App/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-builder.Services.AddSingleton(sp => new CoptClient("http://127.0.0.1:5289"));
+builder.Services.AddSingleton(sp => new CoptClient());
 
 // Initialize CoptLib
 CoptLib.Scripting.DotNetScript.Register();
