bbd1a2851bba2c70f1e8b1ac8b21b76cd7ebd98b
Yoshi Askharoun <jjask7@gmail.com>
2024-02-28 19:59:21 -0600

    Add content endpoint

---
+++ CopticChanter.WebApi/Controllers/ContentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CopticChanter.WebApi.Controllers;
+
+[Route("/content")]
+public class ContentController : Controller
+{
+    [HttpGet]
+    public IActionResult AvailableContent()
+    {
+        return Ok(WebApi.AvailableContent.List);
+    }
+}
