d13e3c5c3b7021883f119592e08901af52a5fa5c
Yoshi Askharoun <jjask7@gmail.com>
2024-03-03 11:01:44 -0600

    Add content sources

---
+++ CopticChanter.WebApi/ContentSources/EnvironmentContentSource.cs
@@ -0,0 +1,36 @@
+﻿using CoptLib.IO;
+using CoptLib.Models.Sequences;
+using CoptLib.Models;
+using System.Xml.Linq;
+
+namespace CopticChanter.WebApi.ContentSources;
+
+public class EnvironmentContentSource(IWebHostEnvironment _env, ILoadContext _context) : IContentSource
+{
+    public Task<Doc> GetDocAsync(string id)
+    {
+        return Task.Run(() => {
+            var stream = AvailableContent.Open("DOC", id, _env);
+            return _context.LoadDoc(stream);
+        });
+    }
+
+    public async Task<DocSet> GetSetAsync(string id)
+    {
+        var stream = AvailableContent.Open("SET", id, _env);
+
+        using var setArchive = SharpCompress.Archives.Zip.ZipArchive.Open(stream);
+        using var setFolder = new OwlCore.Storage.SharpCompress.ReadOnlyArchiveFolder(setArchive, id, id);
+        DocSetReader setReader = new(setFolder, _context);
+        await setReader.ReadDocs();
+
+        return setReader.Set;
+    }
+
+    public async Task<Sequence> GetSequenceAsync(string id)
+    {
+        var stream = AvailableContent.Open("SEQ", id, _env);
+        var seqXml = await XDocument.LoadAsync(stream, LoadOptions.None, default);
+        return SequenceReader.ParseSequenceXml(seqXml, _context);
+    }
+}
---
+++ CopticChanter.WebApi/ContentSources/LoadContextContentSource.cs
@@ -0,0 +1,21 @@
+﻿using CoptLib.IO;
+using CoptLib.Models;
+using CoptLib.Models.Sequences;
+
+namespace CopticChanter.WebApi.ContentSources;
+
+public class LoadContextContentSource(ILoadContext _context) : IModifiableContentSource
+{
+    public async Task AddAsync(Doc doc) => _context.AddDoc(doc);
+
+    public Task AddAsync(DocSet set) => throw new NotSupportedException();
+
+    public Task AddAsync(Sequence seq) => throw new NotSupportedException();
+
+    public async Task<Doc> GetDocAsync(string id) => _context.LoadedDocs.FirstOrDefault(d => d.Key == id)
+        ?? throw new Exception($"No doc with id '{id}' was loaded");
+
+    public Task<Sequence> GetSequenceAsync(string id) => throw new NotSupportedException();
+
+    public Task<DocSet> GetSetAsync(string id) => throw new NotSupportedException();
+}
