c4c68a16ae5fc0978f930c812de4a252192d6650
Yoshi Askharoun <jjask7@gmail.com>
2024-03-10 13:00:47 -0500

    Add transliteration endpoint

---
+++ CopticChanter.WebApi/Controllers/LinguisticsController.cs
@@ -0,0 +1,33 @@
+﻿using CoptLib.Models;
+using CoptLib.Writing;
+using CoptLib.Writing.Linguistics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CopticChanter.WebApi.Controllers;
+
+[Route("/linguistics")]
+[ApiController]
+public class LinguisticsController : ControllerBase
+{
+    [Route("transliterate")]
+    [HttpGet]
+    public IActionResult GetTransliteration([FromQuery] string text,
+        [FromQuery(Name = "dst")] string dstStr, [FromQuery(Name = "src")] string? srcStr,
+        [FromQuery(Name = "syll")] string? syllableSeparator)
+    {
+        if (!LanguageInfo.TryParse(dstStr, out var to))
+            return BadRequest($"Invalid destination language '{dstStr}'");
+
+        LanguageInfo? from;
+        if (srcStr is not null && !LanguageInfo.TryParse(srcStr, out from))
+            return BadRequest($"Invalid source language '{srcStr}'");
+        else if (!LinguisticLanguageService.TryIdentifyLanguage(text, out from))
+            return BadRequest($"Unable to infer source language. Please provide it using the `src` parameter.");
+
+        SimpleContent content = new(text, null);
+
+        var resultDef = LinguisticLanguageService.Default.Transliterate(content, to, from, syllableSeparator);
+        var resultText = ((IContent)resultDef).Inlines.ToString();
+        return Ok(resultText);
+    }
+}
