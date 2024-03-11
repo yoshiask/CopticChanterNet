1a6b53d987a23f82f6ffb0307c5b245bd448f230
Yoshi Askharoun <jjask7@gmail.com>
2024-03-10 19:31:22 -0500

    Add transliteration page

---
+++ CopticChanter.Blazor.App/Pages/Linguistics.razor.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CopticChanter.WebApi.Client;
+using Microsoft.AspNetCore.Components;
+using OwlCore.Extensions;
+
+namespace CopticChanter.Blazor.App.Pages;
+
+public class LinguisticsBase : ComponentBase
+{
+    public LinguisticsBase()
+    {
+        TransliterateCommand = new AsyncRelayCommand(SearchAsync);
+    }
+
+    [Inject]
+    public CoptClient Client { get; set; }
+
+    public string? ErrorMessage { get; set; }
+
+    public string? Response { get; set; }
+
+    public bool Loading { get; set; } = false;
+
+
+    public string Text { get; set; } = "";
+
+    public string DstStr { get; set; } = "English";
+    public string SrcStr { get; set; } = "";
+
+    public IAsyncRelayCommand TransliterateCommand { get; }
+
+    private async Task SearchAsync()
+    {
+        Loading = true;
+        ErrorMessage = null;
+        try
+        {
+            var dst = CoptLib.Writing.LanguageInfo.Parse(DstStr);
+            CoptLib.Writing.LanguageInfo.TryParse(SrcStr, out var src);
+
+            Response = await Client.TransliterateAsync(Text, dst, src);
+        }
+        catch (Flurl.Http.FlurlHttpException httpEx)
+        {
+            Response = null;
+            ErrorMessage = await httpEx.GetResponseStringAsync();
+        }
+        catch (Exception ex)
+        {
+            Response = null;
+            ErrorMessage = ex.ToString();
+        }
+
+        Loading = false;
+        StateHasChanged();
+    }
+
+    protected void BeginTransliteration() => TransliterateCommand.ExecuteAsync(null).Forget();
+}
