08e5b03af36d532dcda8a1bdcb7e0c1d347d9a86
Yoshi Askharoun <jjask7@gmail.com>
2024-03-21 12:59:49 -0500

    Dispose of doc stream after reading

--- CopticChanter.WebApi/ContentSources/EnvironmentContentSource.cs
+++ CopticChanter.WebApi/ContentSources/EnvironmentContentSource.cs
@@ -7,17 +7,15 @@ namespace CopticChanter.WebApi.ContentSources;
 
 public class EnvironmentContentSource(IWebHostEnvironment _env, ILoadContext _context) : IContentSource
 {
-    public Task<Doc> GetDocAsync(string id)
+    public async Task<Doc> GetDocAsync(string id)
     {
-        return Task.Run(() => {
-            var stream = AvailableContent.Open("DOC", id, _env);
-            return _context.LoadDoc(stream);
-        });
+        await using var stream = AvailableContent.Open("DOC", id, _env);
+        return _context.LoadDoc(stream);
     }
 
     public async Task<DocSet> GetSetAsync(string id)
     {
-        var stream = AvailableContent.Open("SET", id, _env);
+        await using var stream = AvailableContent.Open("SET", id, _env);
 
         using var setArchive = SharpCompress.Archives.Zip.ZipArchive.Open(stream);
         using var setFolder = new OwlCore.Storage.SharpCompress.ReadOnlyArchiveFolder(setArchive, id, id);
@@ -29,7 +27,7 @@ public class EnvironmentContentSource(IWebHostEnvironment _env, ILoadContext _co
 
     public async Task<Sequence> GetSequenceAsync(string id)
     {
-        var stream = AvailableContent.Open("SEQ", id, _env);
+        await using var stream = AvailableContent.Open("SEQ", id, _env);
         var seqXml = await XDocument.LoadAsync(stream, LoadOptions.None, default);
         return SequenceReader.ParseSequenceXml(seqXml, _context);
     }
