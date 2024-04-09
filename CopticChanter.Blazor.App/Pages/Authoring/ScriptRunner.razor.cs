5167e8b0ffa6d59d7d5148e9e3ac3d3dcde152ae
Yoshi Askharoun <jjask7@gmail.com>
2024-04-09 11:56:43 -0500

    Add authoring and script runner pages

---
+++ CopticChanter.Blazor.App/Pages/Authoring/ScriptRunner.razor.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CommunityToolkit.Mvvm.Input;
+using CopticChanter.WebApi.Client;
+using Microsoft.AspNetCore.Components;
+using OwlCore.Extensions;
+
+namespace CopticChanter.Blazor.App.Pages.Authoring;
+
+public class ScriptRunnerBase : ComponentBase
+{
+    public ScriptRunnerBase()
+    {
+        RunCommand = new AsyncRelayCommand(RunAsync);
+    }
+
+    [Inject]
+    [NotNull]
+    public CoptClient? Client { get; set; }
+
+    public string? ErrorMessage { get; set; }
+
+    public string? Response { get; set; }
+
+    public bool Loading { get; set; }
+
+
+    public string Body { get; set; } = "";
+
+    public string TypeId { get; set; } = "dotnet";
+
+    public IAsyncRelayCommand RunCommand { get; }
+
+    private async Task RunAsync()
+    {
+        Loading = true;
+        ErrorMessage = null;
+        try
+        {
+            Response = await Client.TestScriptAsync(Body, TypeId, null);
+        }
+        catch (Flurl.Http.FlurlHttpException httpEx)
+        {
+            Response = null;
+            ErrorMessage = await httpEx.GetResponseStringAsync();
+        }
+        catch (Exception ex)
+        {
+            Response = null;
+            ErrorMessage = ex.ToString();
+        }
+
+        Loading = false;
+        StateHasChanged();
+    }
+
+    protected void BeginRun() => RunCommand.ExecuteAsync(null).Forget();
+}
