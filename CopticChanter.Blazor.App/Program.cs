7496412142fdfbd32c8dc0a7abdc4713970074c6
Joshua "Yoshi" Askharoun <jjask7@gmail.com>
2025-04-15 00:46:04 -0500

    Update for Pascha Week, allow language settings from url

--- CopticChanter.Blazor.App/Program.cs
+++ CopticChanter.Blazor.App/Program.cs
@@ -8,7 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-builder.Services.AddSingleton(sp => new CoptClient());
+builder.Services.AddSingleton(sp => new CoptClient("https://localhost:7257"));
 
 // Initialize CoptLib
 CoptLib.Scripting.DotNetScript.Register();
